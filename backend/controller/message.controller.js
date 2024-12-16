40ec947a026f674f4668fea399fcaf8af28c0da5
sahilyadav2004 <sahillawaniya4@gmail.com>
2024-12-16 20:13:46 +0530

    backend mess

---
+++ backend/controller/message.controller.js
@@ -0,0 +1,53 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+export const sendMessage = async (req, res) => {
+  //   console.log("messagesend");
+  try {
+    const message = req.body.message;
+    const senderId = req.user._id;
+    const { id: receiverId } = req.params;
+
+    // const conversationId=req.body.conversationId;
+    let conversation = await Conversation.findOne({
+      members: { $all: [senderId, receiverId] },
+    });
+    if (!conversation) {
+      conversation = await Conversation.create({
+        members: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    // await conversation.save();
+    // await newMessage.save();
+    await Promise.all([conversation.save(), newMessage.save()]); //run parallel
+    res.status(201).json({ message: "message sent successfully", newMessage });
+  } catch (error) {
+    console.log("error in send message", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const getMessage = async (req, res) => {
+  try {
+    const senderId = req.user._id;
+    const { id: chatUser } = req.params;
+    let conversation = await Conversation.findOne({
+      members: { $all: [senderId, chatUser] },
+    }).populate("messages");
+    if (!conversation) {
+      return res.status(201).json([]);
+    }
+    const messages = conversation.messages;
+    res.status(201).json(messages);
+  } catch (error) {
+    console.log("error in getting message", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/jwt/generatetoken.js
+++ backend/jwt/generatetoken.js
@@ -1,11 +1,12 @@
 import jwt from "jsonwebtoken"
 
  const createTokenandSaveCookie=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_TOKEN,{expiresIn:"10d"});
-    res.cookie("jwt",token,{
+    const token=jwt.sign({ userId },process.env.JWT_TOKEN,{expiresIn:"10d"});
+    res.cookie('jwt',token,{
         httpOnly:true,
-        secure:false,
-        sameSite:"strict" //csrf
+        secure:true,
+        sameSite:"strict" ,//csrf,
+        path: '/'
     })
 }
 export default createTokenandSaveCookie;
